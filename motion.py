1ae72ae226de7660980e36f313be2df9fb4e752f
Zach <zz@getleaf.co>
2018-12-24 17:34:05 -0700

    Added example doc string for forward method.

---
+++ motion.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+class motion(object):
+    """
+        Class to control the motion of the rover.
+        Attributes:
+            offset: An adjustment to straighten the steering when centered at 90 degrees
+
+        TODO:
+            Implement forward function
+            Implement reverse funciton
+            Implement left steer
+            Implement right steer
+            Implement stop
+            Implement offset
+    """
+
+    def __init__(self):
+        self.offset = 0
+
+    def forward(self, speed):
+        """Move Rover forward.
+
+        Args:
+            speed (int): the speed we want to go forward (0-100).
+        """
+        print("Forward")
+
+    def reverse(self):
+        print("Reverse")
+
+    def stop(self):
+        print("Stop")
+
+    def left(self):
+        print("Left")
+
+    def right(self):
+        print("Right")
+
+    def center(self):
+        print("Center")
+
+if __name__ == '__main__':
+    move = motion()
+    move.forward()
